70e1874f759c28e42c13ffc3def4f26493a6930b
Andrew Guest <andrew@aguest.me>
2023-08-13 08:26:15 -0500

    Add more solutions and solution overviews

--- project-euler/problem 3/solution.py
+++ project-euler/problem 3/solution.py
@@ -1,7 +1,14 @@
-def is_prime(num: int) -> bool:
-    if num == 1:
-        return False
-    elif num > 1:
-        for i in range(2, num):
-            if (num % i) == 0:
-                return True
+def prime_factor(number):
+    largest_factor = 1
+    c = number
+    i = 2
+
+    while c > 1:
+        if c % i == 0:
+            largest_factor = i
+            c = c // i
+        i += 1
+    print(largest_factor)
+
+
+prime_factor(600851475143)
