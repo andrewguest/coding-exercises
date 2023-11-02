39e4bd2883abff97277ec7ed887bfccf14255c70
Andrew Guest <andrew@aguest.me>
2023-11-01 22:04:32 -0500

    Added some comments

--- project-euler/problem 16/solution.py
+++ project-euler/problem 16/solution.py
@@ -1,1 +1,6 @@
+#   l = []
+#   for d in str(2 ** 1000):
+#       l.append(int(d))
+#   print(sum(l))
+
 print(sum([int(d) for d in str(2**1000)]))
--- project-euler/problem 20/solution.py
+++ project-euler/problem 20/solution.py
@@ -1,4 +1,7 @@
 import math
 
-
+#   l = []
+#   for d in str(math.prod(range(1, 101))):
+#       l.append(int(d))
+#   print(sum(l))
 print(sum([int(d) for d in str(math.prod(range(1, 101)))]))
