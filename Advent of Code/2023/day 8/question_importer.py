25601202d531a8a0c4c1c110c0f33de4a0120882
Andrew Guest <andrew@aguest.me>
2023-12-10 18:54:23 -0600

    Day 8

---
+++ Advent of Code/2023/day 8/question_importer.py
@@ -0,0 +1,7 @@
+def parse_question_input(input_filename: str = "input.txt") -> list[str]:
+    lines: list[str] = []
+
+    with open(input_filename, "r") as file:
+        lines = file.read().splitlines()
+
+    return lines
---
+++ Advent of Code/2023/day 8/solution.py
@@ -0,0 +1,92 @@
+import math
+import re
+
+
+def part_1():
+    input_list = open("input.txt").read().splitlines()
+    node_list = {}
+    direction = list(input_list[0])
+    curr = "AAA"
+    steps = 0
+
+    for i in input_list[2:]:
+        a = re.split(r"[\W]+", i)
+        node_list[a[0]] = (a[1], a[2])
+
+    while True:
+        for u in direction:
+            steps += 1
+            node_tuple = node_list.get(curr)
+            if u == "L":
+                curr = node_tuple[0]
+            else:
+                curr = node_tuple[1]
+
+            if curr == "ZZZ":
+                break
+
+        else:
+            continue
+        break
+
+    print(steps)
+
+
+def part_2():
+    input_list = open("input.txt").read().splitlines()
+    node_dict = {}
+    direction = list(input_list[0])
+    steps = 0
+    ghost_list = []
+    math_list = []
+    loop_list = []
+    max_steps = 1
+
+    for i in input_list[2:]:
+        a = re.split(r"[\W]+", i)
+        node_dict[a[0]] = (a[1], a[2])
+
+    for x in node_dict:
+        if x[-1] == "A":
+            ghost_list.append(x)
+
+    for _ in range(len(ghost_list) * 2):
+        math_list.append("")
+
+    for _ in range(len(ghost_list)):
+        loop_list.append("")
+
+    while True:
+        for u in direction:
+            steps += 1
+            index = 0
+            for p in ghost_list:
+                if p[-1] == "Z" and math_list[index] != "" and loop_list[index] == "":
+                    loop_list[index] = steps - math_list[index + len(ghost_list)]
+
+                if p[-1] == "Z" and math_list[index] == "":
+                    math_list[index] = p
+                    math_list[index + len(ghost_list)] = steps
+
+                node_tuple = node_dict.get(p)
+                if u == "L":
+                    ghost_list[index] = node_tuple[0]
+                else:
+                    ghost_list[index] = node_tuple[1]
+
+                index += 1
+
+            if "" not in loop_list:
+                for c in loop_list:
+                    max_steps = math.lcm(c, max_steps)
+                print(max_steps)
+                break
+
+        else:
+            continue
+        break
+
+
+if __name__ == "__main__":
+    part_1()
+    part_2()
