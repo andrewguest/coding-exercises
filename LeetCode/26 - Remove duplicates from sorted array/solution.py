76db32d8d08a6dfdbcd2a027a92459dabc6bda26
Andrew Guest <andrew@aguest.me>
2025-11-27 18:20:44 -0600

    Clean up old exercises

--- LeetCode/26 - Remove duplicates from sorted array/solution.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def removeDuplicates(self, nums: list[int]) -> int:
-
-        # nums[:]  -> replace a list in place
-        nums[:] = sorted(set(nums))
-
-        return len(nums)
--- LeetCode/26 - Remove duplicates from sorted array/solution.swift
+++
@@ -1,8 +0,0 @@
-class Solution {
-    func removeDuplicates(_ nums: inout [Int]) -> Int {
-        nums = Array(Set(nums))
-        nums.sort{$0 < $1}
-
-        return nums.count
-    }
-}
